e70496dc435042de456f0b7d531fd6097a0f3368
Novojit Saha <n.saha@student.utwente.nl>
2023-12-05 00:10:43 +0100

    - Day 1 Part 1 - Update .gitignore for js files

--- day1/trebuchet.ts
+++ day1/trebuchet.ts
@@ -1,1 +1,51 @@
-let count : number = 0;
+import * as fs from 'fs';
+
+const inputDir : string = './day1/input.txt';
+
+function calculate(input: string) : number{
+
+    
+    
+    const lines : string[] = input.split('\n');
+
+    let counter : number = 0;
+
+    for (const line of lines){
+
+        let numArray : number[] = [];
+
+        for(const char of line){
+            if(!isNaN(parseInt(char))){
+
+                numArray.push(parseInt(char));
+                
+            }
+        }
+
+        let len : number = numArray.length; 
+
+        
+        let result : string = `${numArray[0]}${numArray[len - 1]}`
+
+        counter += parseInt(result);
+        
+    }
+
+    return counter;
+}
+
+
+
+// read input file
+fs.readFile(inputDir, 'utf8', (err, data) => {
+
+    if(err){
+       console.error('Error reading the file: ', err);
+        return;
+    } 
+    
+    console.log(calculate(data));
+    
+})
+
+
